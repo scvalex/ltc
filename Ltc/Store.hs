b799b2b4306e779f0c1b5ef78e1d0a411405dfd2
Alexandru Scvortov <scvalex@gmail.com>
2012-11-15 02:31:32 +0000

    interface sketc for LTc store

--- Ltc/Store.hs
+++ Ltc/Store.hs
@@ -1,5 +1,22 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module Ltc.Store (
         Store(..)
     ) where
 
-data Store = Store
+import Data.ByteString ( ByteString )
+
+type Key = String
+type Value = ByteString
+type Version = Int
+
+class Store a where
+    data Handle a :: *
+
+    connect :: a -> IO (Maybe (Handle a))
+    close :: Handle a -> IO (Maybe ())
+
+    get :: Handle a -> Key -> Version -> IO (Maybe Value)
+    getLatest :: Handle a -> Key -> IO (Maybe (Value, Version))
+    set :: Handle a -> Key -> Value -> IO (Maybe Version)
+    del :: Handle a -> Key -> IO (Maybe Version)
