aae94f711a7b4ccdf85aaf30923d33195bf9cd60
Alexandru Scvortov <scvalex@gmail.com>
2013-01-26 13:48:20 +0000

    add QC random tester for data store

--- Test/StoreProps.hs
+++ Test/StoreProps.hs
@@ -1,37 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Main where
 
 import Ltc.Store
 
-import Control.Exception ( finally )
+import Control.Applicative
 import Control.Monad
+import Data.ByteString.Lazy.Char8 ( ByteString )
+import qualified Data.ByteString.Lazy.Char8 as BL
+import Data.Foldable ( foldlM )
+import qualified Data.Map as M
 import Data.Monoid
 import qualified Data.VectorClock as VC
 import System.Directory
 
 import Test.Framework
 import Test.Framework.Providers.HUnit
--- import Test.Framework.Providers.QuickCheck2
+import Test.Framework.Providers.QuickCheck2
 import Test.HUnit
--- import Test.QuickCheck
-
---FIXME Add QC test Simple test-suite
+import Test.QuickCheck
+import Test.QuickCheck.Monadic as QCM
 
 main :: IO ()
 main = defaultMainWithOpts
        [ testCase "open" testOpen
        , testCase "simpleSetGet" testSimpleSetGet
        , testCase "simpleHistory" testSimpleHistory
---       , testProperty "idemParseShowStatement" prop_IdemParseShowStatement
-       ] opts
-  where
-    opts = mempty {
-             ropt_test_options =
-                 Just (mempty
-                       { topt_maximum_generated_tests            = Just 500
-                       , topt_maximum_unsuitable_generated_tests = Just 5000
-                       })}
+       , testProperty "setGetLatest" propSetGetLatest
+       ] mempty
 
 --------------------------------
 -- Unit tests
@@ -77,6 +74,56 @@ testSimpleHistory = cleanEnvironment ["test-store"] $ do
     res3 @?= Just "bar"
 
 --------------------------------
+-- QuickCheck
+--------------------------------
+
+data Command = GetLatest Key | Set Key Value
+             deriving ( Show )
+
+newtype Commands = Commands { unCommands :: [Command] }
+                 deriving ( Show )
+
+instance Arbitrary ByteString where
+    arbitrary = sized $ \n -> do
+        BL.pack <$> sequence [ choose (' ', '~') | _ <- [1..n] ]
+
+instance Arbitrary Commands where
+    arbitrary = sized $ \n -> do
+        let kn = ceiling (sqrt (fromIntegral n :: Double)) :: Int
+        keys <- replicateM kn arbitrary
+        Commands <$> replicateM n (makeCommand keys)
+      where
+        makeCommand keys = do
+            n <- choose (1, 2 :: Int)
+            let key = elements keys
+            case n of
+                1 -> GetLatest <$> key
+                2 -> Set <$> key <*> arbitrary
+                _ -> fail "unknown case in 'Arbitrary Command'"
+
+propSetGetLatest :: Commands -> Property
+propSetGetLatest cmds = monadicIO $ do
+    cleanEnvironmentP ["test-store"] $ do
+        store <- run $ open (OpenParameters { location       = "test-store"
+                                            , useCompression = False
+                                            , nodeName       = "test" })
+        _ <- foldlM (runCmd store) M.empty (unCommands cmds)
+        run $ close store
+  where
+    runCmd store kvs cmd = do
+        case cmd of
+            GetLatest key -> do
+                res <- run $ getLatest store key
+                QCM.assert (res == M.lookup key kvs)
+                return kvs
+            Set key value -> do
+                vsn <- run $ set store key value
+                case M.lookup key kvs of
+                    Nothing          -> return ()
+                    Just (_, vsnOld) -> QCM.assert (vsnOld `VC.causes` vsn)
+                return (M.insert key (value, vsn) kvs)
+
+--------------------------------
 -- Helpers
 --------------------------------
 
@@ -92,5 +139,9 @@ rmrf fp = do
 cleanEnvironment :: [FilePath] -> Assertion -> Assertion
 cleanEnvironment files ass = do
     mapM_ rmrf files
-    -- FIXME Re-enable cleanup after tests.
-    ass `finally` return () --mapM_ rmrf files
+    ass
+
+cleanEnvironmentP :: [FilePath] -> PropertyM IO a -> PropertyM IO a
+cleanEnvironmentP files prop = do
+    run $ mapM_ rmrf files
+    prop
