bbcb4a0ee7f70ab515ef25e8d4a2fc552b2f7cec
Alexandru Scvortov <scvalex@gmail.com>
2013-01-31 13:52:47 +0000

    first couple of redis commands

--- Ltc/Adapter/RedisAdapter.hs
+++ Ltc/Adapter/RedisAdapter.hs
@@ -2,13 +2,22 @@ module Ltc.Adapter.RedisAdapter (
         redisProxyD
     ) where
 
+import Control.Monad ( unless )
 import Control.Proxy
 import Ltc.Store
-import Network.Redis
+import Network.Redis ( RedisMessage(..) )
 
 redisProxyD :: (Proxy p, Store s) => s -> () -> Pipe p RedisMessage RedisMessage IO ()
-redisProxyD store () = runIdentityP $ forever $ do
+redisProxyD store () = runIdentityP loop
+  where
+    loop = do
     cmd <- request ()
-    let reply = cmd
-    lift $ print reply
+    (reply, stop) <- case cmd of
+        MultiBulk [Bulk "PING"] ->
+            return (Status "PONG", False)
+        MultiBulk [Bulk "QUIT"] ->
+            return (Status "OK", True)
+        _ ->
+            return (Error "ERR unknown command", False)
     respond reply
+    unless stop loop
