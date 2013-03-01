a66e01efcd6fe0cacda461615808068b9c8768bb
Alexandru Scvortov <scvalex@gmail.com>
2013-03-01 00:14:50 +0000

    move serialization specific bits to their own module

--- ltc-tool.hs
+++ ltc-tool.hs
@@ -1,31 +1,27 @@
-{-# LANGUAGE DeriveDataTypeable, DeriveGeneric, FlexibleContexts #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 module Main where
 
 import Control.Applicative ( (<$>) )
 import Control.Concurrent.MVar ( newEmptyMVar, putMVar, takeMVar )
 import Data.Foldable ( foldlM )
-import Data.Map ( Map )
-import Data.Traversable ( forM )
 import Data.Version ( showVersion )
-import GHC.Generics ( Generic )
 import Language.Sexp ( Sexpable(..), printHum )
 import Ltc.Store
-import Ltc.Store.Diff ( Diff, Diffable(..) )
+import Ltc.Store.Serialization ( getDiffPack )
 import Network.BSD ( getHostName )
 import Network.RedisServer ( serve )
 import Paths_ltc ( version )
+import qualified Control.Exception as CE
+import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.Set as S
 import System.Console.CmdArgs
 import System.Posix.Signals ( Handler(..), installHandler, sigINT )
 import Text.Printf ( printf )
-import qualified Control.Exception as CE
-import qualified Data.ByteString.Lazy.Char8 as BL
-import qualified Data.Map as M
-import qualified Data.Set as S
 
 data Modes = Fsck { dir :: FilePath }
            | Info { dir :: FilePath }
-           | DiffPack { dir :: FilePath }
+           | Export { dir :: FilePath }
            | Redis { dir :: FilePath }
            deriving ( Show, Data, Typeable )
 
@@ -35,7 +31,7 @@ ltcModes =
       &= help "check the integrity of a store"
     , Info { dir = def &= typDir &= argPos 1 }
       &= help "list information about a store"
-    , DiffPack { dir = def &= typDir &= argPos 1 }
+    , Export { dir = def &= typDir &= argPos 1 }
       &= help "dump a store to a single file"
     , Redis { dir = "redis-store" &= typDir }
       &= help "run a store with a Redis interface"
@@ -62,11 +58,10 @@ main = do
             vn <- foldlM (\n k -> maybe n ((n+) . length) <$> keyVersions store k) 0 ks
             _ <- printf "  values   : %d\n" vn
             close store
-        DiffPack d -> do
+        Export d -> do
             hostname <- getHostName
             store <- open (openParameters d hostname)
-            ks <- keys store
-            dp <- foldlM (addKeyHistory store) (Diffs M.empty) ks
+            dp <- getDiffPack store
             BL.putStrLn (printHum (toSexp dp))
             close store
         Redis d -> do
@@ -88,56 +83,3 @@ main = do
         OpenParameters { location       = d
                        , useCompression = False
                        , nodeName       = (BL.pack hostname) }
-
-    addKeyHistory store (Diffs m) key = do
-        kh <- getKeyHistory store key
-        return (Diffs (M.insert key kh m))
-
-    getKeyHistory :: (Store s) => s -> Key -> IO KeyHistory
-    getKeyHistory store key = do
-        ty <- storeUnJust =<< keyType store key
-        case ty of
-            SingleInteger -> do
-                (tip :: Value (Single Integer), _) <- storeUnJust =<< getLatest store key
-                diffs <- getDiffs store key tip
-                return (IntKeyHistory tip diffs)
-            CollectionInteger -> do
-                (tip :: Value (Collection Integer), _) <- storeUnJust =<< getLatest store key
-                diffs <- getDiffs store key tip
-                return (IntSetKeyHistory tip diffs)
-            SingleString -> do
-                (tip :: Value (Single BL.ByteString), _) <- storeUnJust =<< getLatest store key
-                diffs <- getDiffs store key tip
-                return (StringKeyHistory tip diffs)
-            CollectionString -> do
-                (tip :: Value (Collection BL.ByteString), _) <- storeUnJust =<< getLatest store key
-                diffs <- getDiffs store key tip
-                return (StringSetKeyHistory tip diffs)
-
-    getDiffs :: (Store s, ValueString (Value a), Diffable a)
-             => s -> Key -> Value a -> IO [Diff a]
-    getDiffs store key tip = do
-        vsns <- storeUnJust =<< keyVersions store key
-        -- @vsns@ contains at least the tip.
-        vs <- forM (tail vsns) (\vsn -> storeUnJust =<< get store key vsn)
-        let (_, diffs) = foldl (\(v, ds) v' -> (v', reverseDiff (diffFromTo v v') : ds))
-                               (tip, [])
-                               vs
-        return diffs
-
-    storeUnJust (Just a) = return a
-    storeUnJust Nothing  = fail "could not find expected value in store"
-
-data KeyHistory = IntKeyHistory (Value (Single Integer)) [Diff (Single Integer)]
-                | IntSetKeyHistory (Value (Collection Integer)) [Diff (Collection Integer)]
-                | StringKeyHistory (Value (Single BL.ByteString)) [Diff (Single BL.ByteString)]
-                | StringSetKeyHistory (Value (Collection BL.ByteString))
-                                      [Diff (Collection BL.ByteString)]
-                deriving ( Generic )
-
-instance Sexpable KeyHistory
-
-data Diffs = Diffs (Map Key KeyHistory)
-           deriving ( Generic )
-
-instance Sexpable Diffs
---
+++ src/Ltc/Store/Serialization.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE DeriveGeneric, FlexibleContexts #-}
+
+module Ltc.Store.Serialization (
+        DiffPack(..),
+
+        KeyHistory(..), getDiffPack
+    ) where
+
+import Control.Applicative ( (<$>) )
+import Control.Monad ( forM )
+import Data.ByteString.Lazy.Char8 ( ByteString )
+import Data.Foldable ( foldlM )
+import Data.Map ( Map )
+import GHC.Generics ( Generic )
+import Language.Sexp ( Sexpable(..) )
+import Ltc.Store.Class ( Store(..), Key
+                       , Value(..), ValueString(..), Type(..), Single, Collection )
+import Ltc.Store.Diff ( Diff, Diffable(..) )
+import qualified Data.Map as M
+import Text.Printf ( printf )
+
+----------------------
+-- Wrappers around values and diffs
+----------------------
+
+-- | 'KeyHistory' achieves two goals.  First, it solidifies the type parameter of 'Value'
+-- by encoding the possibilities as a sum type.  Second, it encapsulates the current value
+-- associated with a key, and the history of changes leading up to that point.
+data KeyHistory = IntKeyHistory (Value (Single Integer)) [Diff (Single Integer)]
+                | IntSetKeyHistory (Value (Collection Integer)) [Diff (Collection Integer)]
+                | StringKeyHistory (Value (Single ByteString)) [Diff (Single ByteString)]
+                | StringSetKeyHistory (Value (Collection ByteString))
+                                      [Diff (Collection ByteString)]
+                deriving ( Generic )
+
+instance Sexpable KeyHistory
+
+-- | 'DiffPack' is just a map of 'Key's to 'KeyHistory's.
+data DiffPack = DiffPack (Map Key KeyHistory)
+              deriving ( Generic )
+
+instance Sexpable DiffPack
+
+----------------------
+-- Helpers
+----------------------
+
+-- | Get all the data from a store.
+getDiffPack :: (Store s) => s -> IO DiffPack
+getDiffPack store = do
+    ks <- keys store
+    DiffPack <$> foldlM addKeyHistory M.empty ks
+  where
+    addKeyHistory m key = do
+        kh <- getKeyHistory store key
+        return (M.insert key kh m)
+
+-- | Get the entire history of a key.
+getKeyHistory :: (Store s) => s -> Key -> IO KeyHistory
+getKeyHistory store key = do
+    ty <- storeUnJust =<< keyType store key
+    case ty of
+        SingleInteger -> do
+            (tip :: Value (Single Integer), _) <- storeUnJust =<< getLatest store key
+            diffs <- getDiffs tip
+            return (IntKeyHistory tip diffs)
+        CollectionInteger -> do
+            (tip :: Value (Collection Integer), _) <- storeUnJust =<< getLatest store key
+            diffs <- getDiffs tip
+            return (IntSetKeyHistory tip diffs)
+        SingleString -> do
+            (tip :: Value (Single ByteString), _) <- storeUnJust =<< getLatest store key
+            diffs <- getDiffs tip
+            return (StringKeyHistory tip diffs)
+        CollectionString -> do
+            (tip :: Value (Collection ByteString), _) <- storeUnJust =<< getLatest store key
+            diffs <- getDiffs tip
+            return (StringSetKeyHistory tip diffs)
+  where
+    getDiffs :: (ValueString (Value a), Diffable a)
+             => Value a -> IO [Diff a]
+    getDiffs tip = do
+        vsns <- storeUnJust =<< keyVersions store key
+        -- @vsns@ contains at least the tip.
+        vs <- forM (tail vsns) (\vsn -> storeUnJust =<< get store key vsn)
+        let (_, diffs) = foldl (\(v, ds) v' -> (v', reverseDiff (diffFromTo v v') : ds))
+                               (tip, [])
+                               vs
+        return diffs
+
+    storeUnJust (Just a) = return a
+    storeUnJust Nothing  = fail (printf "could not find value for %s in store" (show key))
