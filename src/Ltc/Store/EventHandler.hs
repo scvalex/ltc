b0c8eca0112f47ee4c598b7ddbf67385481e0ba4
Alexandru Scvortov <scvalex@gmail.com>
2013-05-25 22:54:21 +0100

    basic docs for EventHandler

--- src/Ltc/Store/EventHandler.hs
+++ src/Ltc/Store/EventHandler.hs
@@ -3,7 +3,10 @@ module Ltc.Store.EventHandler (
         EventHandler(..), Event(..)
     ) where
 
+-- | An event signals that something has happened inside an "Ltc.Store".
 data Event = GenericEvent String
 
+-- | An 'EventHandler' is something that consumes 'Event's.  This is just the type-class;
+-- see the other modules for actual event handlers.
 class EventHandler a where
     handleEvent :: a -> Event -> IO ()
