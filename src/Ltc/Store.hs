b584e5e4896aee61e681c82814127e5e36cb711e
Alexandru Scvortov <scvalex@gmail.com>
2013-05-25 22:54:01 +0100

    export EventHandler through Ltc.Store

--- src/Ltc/Store.hs
+++ src/Ltc/Store.hs
@@ -1,7 +1,9 @@
 module Ltc.Store (
         module Ltc.Store.Class,
-        module Ltc.Store.Simple
+        module Ltc.Store.Simple,
+        module Ltc.Store.EventHandler
     ) where
 
 import Ltc.Store.Class
+import Ltc.Store.EventHandler
 import Ltc.Store.Simple
