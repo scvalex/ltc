0c32f459aa5cafd91837fc95b9c5d4c3af7118c9
Alexandru Scvortov <scvalex@gmail.com>
2013-05-26 14:59:36 +0100

    start Snap server as part of status plugin

--- src/Network/StatusServer.hs
+++ src/Network/StatusServer.hs
@@ -7,10 +7,17 @@ module Network.StatusServer (
         serve
     ) where
 
+import Control.Concurrent ( forkIO )
 import Control.Exception ( Exception )
 import Data.Typeable ( Typeable )
 import Ltc.Store ( Store )
 import Network.Types ( Hostname, Port )
+import Network.WebSockets ( WebSockets, Hybi00 )
+import Network.WebSockets.Snap ( runWebSocketsSnap )
+import qualified Control.Exception as CE
+import Snap.Core ( route )
+import Snap.Http.Server ( httpServe, defaultConfig )
+import Snap.Util.FileServe ( serveFile, serveDirectory )
 import System.Log.Logger ( debugM )
 
 ----------------------
@@ -30,13 +37,29 @@ data Shutdown = Shutdown
 
 instance Exception Shutdown
 
-newtype Status = Status { getShutdown :: IO () }
+data Status = Status
+    { getShutdown :: IO ()
+    }
 
 -- | Start the status interface.
 serve :: (Store s) => s -> IO Status
 serve _store = do
     debugM tag "starting status interface"
-    return (Status { getShutdown = undefined })
+    let handler = route [ ("", indexHandler)
+                        , ("r", resourcesHandler)
+                        , ("status", statusHandler)
+                        ]
+    tid <- forkIO $
+           CE.handle (\(_ :: Shutdown) -> return ()) $ do
+               httpServe defaultConfig handler
+    let status = Status { getShutdown = CE.throwTo tid Shutdown
+                        }
+    return status
+  where
+    indexHandler = serveFile "index.html"
+    resourcesHandler = serveDirectory "r"
+    statusHandler = runWebSocketsSnap $ \_req -> do
+        undefined :: WebSockets Hybi00 ()
 
 -- | Shutdown a running 'Status'.  Idempotent.
 shutdown :: Status -> IO ()
