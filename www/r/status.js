0572274c4f1382c9cb5a9b44ed87e0366db7f836
Alexandru Scvortov <scvalex@gmail.com>
2013-05-28 01:21:31 +0100

    breakdown event counts by second

--- www/r/status.js
+++ www/r/status.js
@@ -6,7 +6,7 @@ function log() {
     }
 }
 
-function LogEntry(msg) {
+function LogEntry(msg, handleEvent) {
     var self = this;
 
     var now = new Date();
@@ -15,11 +15,11 @@ function LogEntry(msg) {
     if (msg.hasOwnProperty("GetEvent")) {
         self.operation = "get";
         self.target = msg["GetEvent"].eventTarget;
-        model.countGets(model.countGets() + 1);
+        handleEvent("get");
     } else if (msg.hasOwnProperty("SetEvent")) {
         self.operation = "set";
         self.target = msg["SetEvent"].eventTarget;
-        model.countSets(model.countSets() + 1);
+        handleEvent("set");
     } else {
         log("got unknown message: ", msg);
     }
@@ -29,8 +29,25 @@ function AppViewModel() {
     var self = this;
 
     self.log = ko.observableArray();
+
     self.countGets = ko.observable(0);
     self.countSets = ko.observable(0);
+    self.eventsPerSec = [];
+    self.eventsCurrentSec = {"get": 0, "set": 0};
+
+    handleEvent = function(type) {
+        if (type == "set") {
+            self.countSets(self.countSets() + 1);
+        } else if (type == "get") {
+            self.countGets(self.countGets() + 1);
+        }
+        self.eventsCurrentSec[type] = self.eventsCurrentSec[type] + 1;
+    }
+
+    setInterval(function () {
+        self.eventsPerSec.push(self.eventsCurrentSec);
+        self.eventsCurrentSec = {"get": 0, "set": 0};
+    }, 1000);
 
     self.socket = new WebSocket(WS_URL, "status");
     self.socket.onopen = function() {
@@ -43,7 +60,7 @@ function AppViewModel() {
         log("websocket closed: ", event)
     }
     self.socket.onmessage = function(event) {
-        self.log.push(new LogEntry(JSON.parse(event.data)));
+        self.log.push(new LogEntry(JSON.parse(event.data), handleEvent));
         // Scroll to bottom
         $("log").scrollTop = $("log").scrollHeight;
     }
