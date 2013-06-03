59b7753a99854dd2640d4455c973748929473b1d
Alexandru Scvortov <scvalex@gmail.com>
2013-06-03 12:22:01 +0100

    a bit more smallcheck

--- test/NodeProtocolProps.hs
+++ test/NodeProtocolProps.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Main where
@@ -7,12 +8,18 @@ import Network.NodeProtocol ( NodeEnvelope(..), NodeMessage(..) )
 import Test.Framework
 import Test.Framework.Providers.SmallCheck
 import Test.SmallCheck
+import Test.SmallCheck.Series
 
 main :: IO ()
 main = defaultMainWithOpts
-       [
+       [ testProperty "encode-decode-node-message/id" encodeDecodeNodeMessageId
        ] mempty
 
 --------------------------------
 -- SmallCheck
 --------------------------------
+
+instance (Monad m) => Serial m NodeMessage
+
+encodeDecodeNodeMessageId :: NodeMessage -> Property IO
+encodeDecodeNodeMessageId _ = undefined
