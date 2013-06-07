790586eb7a8447c8e81dbff789ab8c02ca6fd056
Alexandru Scvortov <scvalex@gmail.com>
2013-06-07 19:55:55 +0100

    make monkey configurable

--- src/Ltc/Monkey.hs
+++ src/Ltc/Monkey.hs
@@ -2,12 +2,17 @@
 
 -- | A monkey is a process which randomly makes changes to the store.
 module Ltc.Monkey (
-        Monkey, start, shutdown
+        -- * Basic interface
+        Monkey, start, shutdown,
+
+        -- * Configuration
+        getDelay, setDelay, getActive, setActive
     ) where
 
-import Control.Concurrent ( forkIO, threadDelay )
+import Control.Concurrent ( forkIO, threadDelay
+                          , MVar, newMVar, readMVar, modifyMVar_ )
 import Control.Exception ( Exception )
-import Control.Monad ( forever )
+import Control.Monad ( forever, when )
 import Data.String ( fromString )
 import Data.Typeable ( Typeable )
 import Ltc.Store ( Store(..), Version )
@@ -34,20 +39,28 @@ data Shutdown = Shutdown
 instance Exception Shutdown
 
 data Monkey = Monkey
-    { getShutdown :: IO ()
+    { getShutdown  :: IO ()
+    , getDelayVar  :: MVar (Double, Double)
+    , getActiveVar :: MVar Bool
     }
 
 -- | Start the monkey on the given store.
 start :: (Store s) => s -> IO Monkey
 start store = do
     debugM tag "starting monkey"
+    delayVar <- newMVar (0.2, 0.4)
+    activeVar <- newMVar True
     tid <- forkIO $
            CE.handle (\(_ :: Shutdown) -> return ()) $ do
                forever $ do
-                   d <- randomRIO (200 * 1000, 400 * 1000) -- 0.2s - 0.4s
+                   (minDelay, maxDelay) <- readMVar delayVar
+                   d <- randomRIO (floor (minDelay * 1000000), floor (maxDelay * 1000000))
                    threadDelay d
-                   accessStoreRandomly
-    let monkey = Monkey { getShutdown = CE.throwTo tid Shutdown
+                   isActive <- readMVar activeVar
+                   when isActive accessStoreRandomly
+    let monkey = Monkey { getShutdown  = CE.throwTo tid Shutdown
+                        , getDelayVar  = delayVar
+                        , getActiveVar = activeVar
                         }
     return monkey
   where
@@ -78,3 +91,20 @@ shutdown :: Monkey -> IO ()
 shutdown monkey = do
     debugM tag "shutting monkey down"
     getShutdown monkey
+
+-- | Get the current delay range.
+getDelay :: Monkey -> IO (Double, Double)
+getDelay = readMVar . getDelayVar
+
+-- | Set the delay range.  This is the amount of seconds the monkey waits between
+-- accesses.
+setDelay :: Monkey -> (Double, Double) -> IO ()
+setDelay monkey delay = modifyMVar_ (getDelayVar monkey) (const (return delay))
+
+-- | Get the current activation state.
+getActive :: Monkey -> IO Bool
+getActive = readMVar . getActiveVar
+
+-- | Set the activation state.  When inactive, the monkey does not touch the store.
+setActive :: Monkey -> Bool -> IO ()
+setActive monkey active = modifyMVar_ (getActiveVar monkey) (const (return active))
